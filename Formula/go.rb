06405eaf9a1d949f189cebaea4e9edc5cc7babdb
Back Yu <szyhf@users.noreply.github.com>
2023-02-09 15:27:56 +0800

    Update go.rb
    try update to go 1.20

--- Formula/go.rb
+++ Formula/go.rb
@@ -1,9 +1,9 @@
 class Go < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.19.5.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.19.5.src.tar.gz"
-  sha256 "8e486e8e85a281fc5ce3f0bedc5b9d2dbf6276d7db0b25d3ec034f313da0375f"
+  url "https://go.dev/dl/go1.20.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.20.src.tar.gz"
+  sha256 "3a29ff0421beaf6329292b8a46311c9fbf06c800077ceddef5fb7f8d5b1ace33"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git", branch: "master"
 
